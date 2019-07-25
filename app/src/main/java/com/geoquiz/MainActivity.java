826e6ce50c433f34b14d5218c2ce0922398e7fbe
KulArtsiom <kulartsiom@gmail.com>
2019-07-25 16:49:35 +0300

    add maket and simple implementation toasts method

--- app/src/main/java/com/geoquiz/MainActivity.java
+++ app/src/main/java/com/geoquiz/MainActivity.java
@@ -1,7 +1,14 @@
 package com.geoquiz;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_true)
+    void onClickTrue() {
+        showToast(this, this.getResources().getString(R.string.correct_toast));
+    }
+
+    @OnClick(R.id.btn_false)
+    void onClickFalse() {
+        showToast(this, this.getResources().getString(R.string.incorrect_toast));
+    }
+
+    private void showToast(Activity activity, String message){
+        Toast.makeText(activity, message, Toast.LENGTH_SHORT).show();
     }
 }
